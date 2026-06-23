ed76998a0956bcf798e81e000a99b68a9462d1b0
Daniil Kirillov <danik.mad@gmail.com>
2026-06-24 00:30:42 +0200

    refactored main to support web module

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ slint::include_modules!();
 mod ui;
 pub mod vehicle;
 pub use vehicle::Vehicle;
+#[cfg(feature = "web")]
+mod web;
+
+#[cfg(feature = "web")]
+#[tokio::main]
+async fn main() {
+    tokio::spawn(async {
+        web::run_web().await;
+    });
+
+    ui::run();
+}
+
+#[cfg(not(feature = "web"))]
 fn main() {
     ui::run();
 }
