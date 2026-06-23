27bd555a602ceddabda939528df29963dfeb404f
Daniil Kirillov <danik.mad@gmail.com>
2026-06-24 00:31:24 +0200

    renamed ui.rs to observer.rs due to extension of web functionality

--- src/observer.rs
+++ src/observer.rs
@@ -1,10 +1,11 @@
-use crate::{Vehicle, vehicle::{IDLE_RATE, MAX_RATE}};
-use rand::{RngExt};
+use crate::Vehicle;
 
 use crate::{AppWindow, CoolerState, DashboardState, FuelState, ShifterState, SpeedometerState, TachometerState};
+#[cfg(feature = "web")]
+use reqwest::Client;
 use slint::{ComponentHandle, Timer, TimerMode};
 
-use std::{sync::{Mutex, OnceLock}, time::Duration};
+use std::{sync::{Mutex, OnceLock}, time::{Duration, Instant}};
 
 static VEHICLE_OBSERVER: OnceLock<Mutex<VehicleObserver>> = OnceLock::new();
 static KEY_HOLD: OnceLock<Mutex<Option<String>>> = OnceLock::new();
@@ -12,32 +13,54 @@ static KEY_HOLD: OnceLock<Mutex<Option<String>>> = OnceLock::new();
 struct VehicleObserver {
     ui: slint::Weak<AppWindow>,
     vehicle: Vehicle,
+    #[cfg(feature = "web")]
+    last_web_update: Instant,
 }
 
 impl VehicleObserver {
     fn new(ui: slint::Weak<AppWindow>, vehicle: Vehicle) -> VehicleObserver {
-        VehicleObserver { ui, vehicle }
-    }
-
-    fn limit_rate(&mut self, rate: f32, target: f32) {
-        const TOLERANCE: f32 = 0.025;
-        if (rate >= target - TOLERANCE) && (rate <= target + TOLERANCE) {
-            let num = rand::rng().random_range(rate * (1.0 - TOLERANCE)..rate * (1.0 + TOLERANCE));
-            self.vehicle.set_rate(num);
-        }
+        VehicleObserver { ui, vehicle, #[cfg(feature = "web")] last_web_update: Instant::now() }
     }
 
     fn update(&mut self) {
         self.key_hold();
-        self.goggle_rate();
+        self.vehicle.goggle_rate();
         if let Some(ui) = self.ui.upgrade() {
+            println!("Updating");
             ui.global::<DashboardState>().set_is_engine_on(self.vehicle.is_on());
             ui.global::<FuelState>().set_fuel(self.vehicle.get_fuel());
             ui.global::<CoolerState>().set_temperature(self.vehicle.get_temperature() as i32);
             ui.global::<TachometerState>().set_rate(self.vehicle.get_rate());
             ui.global::<SpeedometerState>().set_speed(self.vehicle.get_speed() as i32);
             ui.global::<ShifterState>().set_current_gear(self.vehicle.get_gear_str().into());
+            #[cfg(feature = "web")]
+            if self.last_web_update.elapsed() >= Duration::from_millis(100) {
+                self.last_web_update = Instant::now();
+
+                let data = self.vehicle.get_data();
+
+                tokio::spawn(async move {
+                    if let Err(err) = VehicleObserver::send_vehicle_data(data).await {
+                        eprintln!("Failed to send vehicle data: {err:?}");
+                    }
+                });
+            }
         }
+    }
+
+    #[cfg(feature = "web")]
+    async fn send_vehicle_data(data: crate::vehicle::VehicleData) -> Result<(), reqwest::Error> {
+        let client = Client::new();
+
+        let _response = client
+            .post("http://127.0.0.1:3000/data")
+            .json(&data)
+            .send()
+            .await?;
+
+        //println!("POST status: {}", response.status());
+
+        Ok(())
     }
 
     pub fn key_pressed(&mut self, key: &str) {
@@ -47,7 +70,11 @@ impl VehicleObserver {
                 *key_held = Some("w".into());
             }
             "e" => {
-                self.vehicle.ignition();
+                if self.vehicle.is_moving() {
+                    print!("Can't toggle power while moving!");
+                    return;
+                }
+                self.vehicle.toggle_power();
             }
             "s" => {
                 let mut key_held = KEY_HOLD.get().unwrap().lock().unwrap();
@@ -87,20 +114,12 @@ impl VehicleObserver {
                 self.vehicle.brake();
             }
             Some(ref key) if key == "r" => {
-                self.vehicle.refuel(0.1);
+                self.vehicle.refuel(0.02);
             }
             _ => {
                 self.vehicle.idle_brake();
             }
         }
-    }
-
-    fn goggle_rate(&mut self) {
-        if !self.vehicle.is_on() {
-            return;
-        }
-        self.limit_rate(self.vehicle.get_rate(), IDLE_RATE);
-        self.limit_rate(self.vehicle.get_rate(), MAX_RATE);
     }
 }
 
@@ -134,12 +153,12 @@ fn on_quit_requested(ui: &AppWindow) {
     });
 }
 
-fn start_timers() -> [Timer; 1] {
+fn start_timer() -> Timer {
     let timer = Timer::default();
     timer.start(
         TimerMode::Repeated,
         Duration::from_millis(16),
-        move || {
+         move || {
             let mut observer = VEHICLE_OBSERVER
                 .get()
                 .expect("VehicleObserver not initialized")
@@ -148,18 +167,18 @@ fn start_timers() -> [Timer; 1] {
             observer.update();
         },
     );
-    [timer]
+    timer
 }
 
 pub fn run() {
     let window =AppWindow::new().unwrap();
-    VEHICLE_OBSERVER.set(Mutex::new(VehicleObserver::new(window.as_weak(), Vehicle::new())))
+    VEHICLE_OBSERVER.set(Mutex::new(VehicleObserver::new(window.as_weak(), Vehicle::default())))
         .map_err(|_| "VEHICLE_OBSERVER already initialized")
         .unwrap();
     KEY_HOLD.set(Mutex::new(None)).map_err(|_| "KEY_HOLD already initialized").unwrap();
     on_key_pressed(&window);
     on_key_released(&window);
     on_quit_requested(&window);
-    let _timers = start_timers();
+    let _timer = start_timer();
     window.run().unwrap();
 }
